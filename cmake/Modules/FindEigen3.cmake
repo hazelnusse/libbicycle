66c542d1b58c9fde40031e41c2fce5b04f15151b
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-02-22 17:17:33 -0800

    Added DOC property so Eigen dir gets cached.

--- cmake/Modules/FindEigen3.cmake
+++ cmake/Modules/FindEigen3.cmake
@@ -66,6 +66,7 @@ else (EIGEN3_INCLUDE_DIR)
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
+      DOC "Path to Eigen Directory"
     )
 
   if(EIGEN3_INCLUDE_DIR)
